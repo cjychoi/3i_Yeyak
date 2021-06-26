22bfc976182a0873056d9df097e21e208743852e
Jins10 <76994052+Jins10@users.noreply.github.com>
2021-06-26 19:48:33 +0900

    Update home_view.dart

--- mobile/lib/screens/home_view.dart
+++ mobile/lib/screens/home_view.dart
@@ -14,20 +14,6 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-/*
-  final _formKey = GlobalKey<FormState>();
-  final usernameController = TextEditingController();
-  final RegExp checkUsername = RegExp(
-      r"^[a-zA-Z]{1,}(\.)?([a-zA-Z]{1,})?$"); // e.g. juan.kim, andrew.choi
-
-  check() {
-    if (_formKey.currentState == null) return;
-    if (_formKey.currentState!.validate()) {
-      // 검사
-      Navigator.of(context).popAndPushNamed('/home'); // 이동
-    }
-  }
-*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,30 +22,6 @@ class _HomePageState extends State<HomePage> {
             decoration: InputDecoration(
           border: OutlineInputBorder(),
           labelText: 'Enter Name',
-          /* 입력창, validation 적용되어있음.
-          controller: usernameController,
-          validator: (value) {
-            if (value == null || value.isEmpty) {
-              return 'Please type your name';
-            }
-            if (!checkUsername.hasMatch(value)) {
-              return 'Please check your name';
-            }
-            return null;
-          },
-          decoration: InputDecoration(
-            hintText: 'Enter your name',
-            labelText: 'Username',
-            contentPadding:
-                new EdgeInsets.symmetric(vertical: 2.5, horizontal: 1.0),
-            border: OutlineInputBorder(borderRadius: BorderRadius.zero),
-          ),
-          autofocus: true, // 자동으로 포커스 맞춰지게
-          textInputAction: TextInputAction.go, // 키보드 오른쪽 밑 버튼 교체
-          onFieldSubmitted: (value) {
-            // go 버튼 눌렀을때
-            check();
-          },*/
         )),
         backgroundColor: Colors.grey, // App bar 배색
         actions: [
