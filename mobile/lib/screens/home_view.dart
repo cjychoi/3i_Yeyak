cb395751c6c282e1a2cbf0a7912ac4e931b763dc
Jinsang <fldhs2012@gmail.com>
2021-07-04 03:07:29 +0900

    changed date & time picker to a field

--- mobile/lib/screens/home_view.dart
+++ mobile/lib/screens/home_view.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:mobile/screens/login_view.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:flutter/services.dart'; //
+import 'package:intl/intl.dart'; //needed to use date format
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart'; //barcode scanner
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart'; //date picker that supports formfield
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,7 +17,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   //탭바 앱바 밑 바디에 사용하기
   late List<bool> isSelected;
   late TabController _tabController;
-
+  final format = DateFormat("yyyy-MM-dd HH:mm");
   @override
   void initState() {
     super.initState();
@@ -50,120 +51,152 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(
-            Icons.edit,
-            color: Colors.black,
+        resizeToAvoidBottomInset: false, //avoid pixel overloading
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(
+              Icons.edit,
+              color: Colors.black,
+            ),
+            onPressed: () => {
+              //Navigator.pop(context)},
+              Navigator.of(context).popAndPushNamed('/login')
+            },
           ),
-          onPressed: () => {
-            //Navigator.pop(context)},
-            Navigator.of(context).popAndPushNamed('/login')
-          },
-        ),
 
-        backgroundColor: Colors.white, // App bar 배색
-        centerTitle: false,
-        titleSpacing: 0.0,
-        title: Transform(
-          // you can forcefully translate values left side using Transform
-          transform: Matrix4.translationValues(-15.0, 0.0, 0.0),
-          child: new Text(
-            LoginPage.uName,
-            style: new TextStyle(color: Colors.black, fontSize: 25.0),
+          backgroundColor: Colors.white, // App bar 배색
+          centerTitle: false,
+          titleSpacing: 0.0,
+          title: Transform(
+            // you can forcefully translate values left side using Transform
+            transform: Matrix4.translationValues(-15.0, 0.0, 0.0),
+            child: new Text(
+              LoginPage.uName,
+              style: new TextStyle(color: Colors.black, fontSize: 25.0),
+            ),
           ),
+          actions: [
+            IconButton(
+              icon: Image.asset('images/menu.png',
+                  fit: BoxFit.cover, width: 30.0, height: 30.0),
+              onPressed: () => {},
+            ),
+          ],
         ),
-        actions: [
-          IconButton(
-            icon: Image.asset('images/menu.png',
-                fit: BoxFit.cover, width: 30.0, height: 30.0),
-            onPressed: () => {},
-          ),
-        ],
-      ),
 /*
 
           */
-      body: Column(
-        children: <Widget>[
-          //   Image.asset("path"),
-          Container(
-            child: Column(
-              children: <Widget>[
-                Image.asset('images/search_by.png', width: 200),
-                Container(
-                  height: 50,
-                  color: HexColor("0057FF"),
-                  child: TabBar(
-                    tabs: [
-                      Container(
-                        width: 70.0,
-                        child: new Text(
-                          'Device',
-                          style: TextStyle(
-                            fontSize: 20,
-                            //
-                          ),
-                        ),
-                      ),
-                      Container(
-                        width: 70.0,
-                        child: new Text(
-                          'Time',
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                        ),
-                      )
-                    ],
-                    unselectedLabelColor: const Color(0xffacb3bf),
-                    indicatorColor: Colors.white,
-                    labelColor: Colors.white,
-                    indicatorSize: TabBarIndicatorSize.tab,
-                    indicatorWeight: 3.0,
-                    indicatorPadding: EdgeInsets.all(10),
-                    isScrollable: false,
-                    controller: _tabController,
-                  ),
-                ),
-                Container(
-                  height: 100,
-                  child:
-                      TabBarView(controller: _tabController, children: <Widget>[
+        body: new Container(
+          child: ListView(
+            children: <Widget>[
+              //   Image.asset("path"),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset('images/search_by.png', width: 200),
                     Container(
-                      child: IconButton(
-                        icon: Image.asset('images/capture.png',
-                            fit: BoxFit.cover, width: 150.0, height: 150.0),
-                        onPressed: () => {scanQR()},
-                        //Navigator.of(context).popAndPushNamed('/home')},
+                      height: 50,
+                      color: HexColor("0057FF"),
+                      child: TabBar(
+                        tabs: [
+                          Container(
+                            width: 70.0,
+                            child: new Text(
+                              'Device',
+                              style: TextStyle(
+                                fontSize: 20,
+                                //
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 70.0,
+                            child: new Text(
+                              'Time',
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                            ),
+                          )
+                        ],
+                        unselectedLabelColor: const Color(0xffacb3bf),
+                        indicatorColor: Colors.white,
+                        labelColor: Colors.white,
+                        indicatorSize: TabBarIndicatorSize.tab,
+                        indicatorWeight: 3.0,
+                        indicatorPadding: EdgeInsets.all(10),
+                        isScrollable: false,
+                        controller: _tabController,
                       ),
                     ),
                     Container(
-                      child: IconButton(
-                        icon: Icon(Icons.calendar_today_sharp),
-                        iconSize: 70,
-                        onPressed: () {
-                          DatePicker.showDatePicker(context,
-                              showTitleActions: true,
-                              minTime: DateTime(2021, 1, 1),
-                              maxTime: DateTime(2022, 12, 31),
-                              onChanged: (date) {
-                            print('change $date');
-                          }, onConfirm: (date) {
-                            print('confirm $date');
-                          },
-                              currentTime: DateTime.now(),
-                              locale: LocaleType.en);
-                        },
-                      ),
+                      height: 170, //put tabbar below appbar
+                      child: TabBarView(
+                          controller: _tabController,
+                          children: <Widget>[
+                            Container(
+                              child: IconButton(
+                                icon: Image.asset('images/capture.png',
+                                    fit: BoxFit.cover,
+                                    width: 150.0,
+                                    height: 150.0),
+                                onPressed: () => {scanQR()},
+                                //Navigator.of(context).popAndPushNamed('/home')},
+                              ),
+                            ),
+                            Column(children: <Widget>[
+                              Text('Rent date & time: (${format.pattern})'),
+                              DateTimeField(
+                                format: format,
+                                onShowPicker: (context, currentValue) async {
+                                  final date = await showDatePicker(
+                                      context: context,
+                                      firstDate: DateTime(2021, 1, 1),
+                                      initialDate:
+                                          currentValue ?? DateTime.now(),
+                                      lastDate: DateTime(2022, 12, 31));
+                                  if (date != null) {
+                                    final time = await showTimePicker(
+                                      context: context,
+                                      initialTime: TimeOfDay.fromDateTime(
+                                          currentValue ?? DateTime.now()),
+                                    );
+                                    return DateTimeField.combine(date, time);
+                                  } else {
+                                    return currentValue;
+                                  }
+                                },
+                              ),
+                              Text('Return date & time: (${format.pattern})'),
+                              DateTimeField(
+                                format: format,
+                                onShowPicker: (context, currentValue) async {
+                                  final date = await showDatePicker(
+                                      context: context,
+                                      firstDate: DateTime(2021, 1, 1),
+                                      initialDate:
+                                          currentValue ?? DateTime.now(),
+                                      lastDate: DateTime(2022, 12, 31));
+                                  if (date != null) {
+                                    final time = await showTimePicker(
+                                      context: context,
+                                      initialTime: TimeOfDay.fromDateTime(
+                                          currentValue ?? DateTime.now()),
+                                    );
+                                    return DateTimeField.combine(date, time);
+                                  } else {
+                                    return currentValue;
+                                  }
+                                },
+                              ),
+                            ]),
+                          ]),
                     ),
-                  ]),
+                  ],
                 ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+              )
+            ],
+          ),
+        ));
   }
 }
