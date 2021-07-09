dfb266853ae42e0c245cc4e509e8d79bbc59d9fa
James Oh <fldhs2012@gmail.com>
2021-07-09 22:12:20 +0900

    re-formatted my reservation view and reservation list

--- mobile/lib/screens/reservation_list_view.dart
+++ mobile/lib/screens/reservation_list_view.dart
@@ -3,63 +3,151 @@ import 'package:flutter/cupertino.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:mobile/globals.dart' as globals;
 
-class ListPage extends StatefulWidget {
+void main() => runApp(ListPage());
+
+class ListPage extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  _ListPageState createState() => _ListPageState();
+  Widget build(BuildContext context) {
+    return DataTableDemo();
+  }
 }
 
-class _ListPageState extends State<ListPage> {
+class User {
+  String devices;
+  String date;
+  //String returnDate;
+  String userName;
+
+  User({required this.devices, required this.date, required this.userName});
+
+  static List<User> getUsers() {
+    return <User>[
+      User(date: "2021-07-21 05:30", devices: "S 10", userName: "Juan"),
+      User(date: "2021-07-21 06:30", devices: "Note 20", userName: "James"),
+      User(date: "2021-07-20 04:30", devices: "iPhone 11", userName: "George"),
+      User(date: "2021-07-26 07:30", devices: "Juan 12", userName: "Andrew"),
+      User(date: "2021-07-23 04:30", devices: "Juan 84", userName: "Jedi"),
+    ];
+  }
+}
+
+class DataTableDemo extends StatefulWidget {
+  DataTableDemo() : super();
+
+  final String title = "Reservation Status";
+
+  @override
+  DataTableDemoState createState() => DataTableDemoState();
+}
+
+class DataTableDemoState extends State<DataTableDemo> {
+  late List<User> users;
+  late List<User> selectedUsers;
+  late bool sort;
+
   @override
   void initState() {
+    sort = false;
+    selectedUsers = [];
+    users = User.getUsers();
     super.initState();
+  }
+
+  onSortColum(int columnIndex, bool ascending) {
+    if (columnIndex == 0) {
+      if (ascending) {
+        users.sort((a, b) => a.date.compareTo(b.date));
+      } else {
+        users.sort((a, b) => b.date.compareTo(a.date));
+      }
+    }
+  }
+
+  onSelectedRow(bool selected, User user) async {
+    setState(() {
+      if (selected) {
+        selectedUsers.add(user);
+      } else {
+        selectedUsers.remove(user);
+      }
+    });
+  }
+
+  SingleChildScrollView dataBody() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: DataTable(
+        sortAscending: sort,
+        sortColumnIndex: 0,
+        columns: [
+          DataColumn(
+              label: Icon(Icons.calendar_today, color: Colors.green[500]),
+              numeric: false,
+              tooltip: "This is Date",
+              onSort: (columnIndex, ascending) {
+                setState(() {
+                  sort = !sort;
+                });
+                onSortColum(columnIndex, ascending);
+              }),
+          DataColumn(
+            label: Icon(Icons.phone_android, color: Colors.green[500]),
+            numeric: false,
+            tooltip: "This is Device Name",
+          ),
+          DataColumn(
+            label: Icon(Icons.person, color: Colors.green[500]),
+            numeric: false,
+            tooltip: "This is the name of the user",
+          )
+        ],
+        rows: users
+            .map(
+              (user) => DataRow(selected: selectedUsers.contains(user), cells: [
+                DataCell(
+                  Text(user.date),
+                  onTap: () {
+                    print('Selected ${user.date}');
+                  },
+                ),
+                DataCell(
+                  Text(user.devices),
+                ),
+                DataCell(
+                  Text(user.userName),
+                ),
+              ]),
+            )
+            .toList(),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: HexColor("0057FF"),
-          leading: BackButton(
-            //implement backbutton in
-            color: Colors.white,
-          ),
-          title: Text(globals.user!.username),
-          centerTitle: true,
+      appBar: AppBar(
+        leading: BackButton(
+          //onPressed: () {
+          //   Navigator.of(context).popAndPushNamed('/home');
+          // },
+          //implement backbutton in
+          color: Colors.white,
         ),
-        body: DataTable(
-          columns: [
-            DataColumn(
-              label: Icon(Icons.phone_android, color: Colors.green[500]),
-            ),
-            DataColumn(
-              label: Icon(Icons.calendar_today, color: Colors.green[500]),
-            ),
-            DataColumn(
-              label: Icon(Icons.person, color: Colors.green[500]),
-            )
-          ],
-          rows: [
-            DataRow(cells: [
-              DataCell(Text('Galaxy S10')),
-              DataCell(Text('2010-5-14 5:10')),
-              DataCell(Text('James'))
-            ]),
-            DataRow(cells: [
-              DataCell(Text('Galaxy S9')),
-              DataCell(Text('2010-5-14 5:10')),
-              DataCell(Text('Andrew'))
-            ]),
-            DataRow(cells: [
-              DataCell(Text('iPhone 11')),
-              DataCell(Text('2010-5-14 5:10')),
-              DataCell(Text('George'))
-            ]),
-            DataRow(cells: [
-              DataCell(Text('iPHone SE2')),
-              DataCell(Text('2010-5-14 5:10')),
-              DataCell(Text('Juan'))
-            ]),
-          ],
-        ));
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        verticalDirection: VerticalDirection.down,
+        children: <Widget>[
+          Expanded(
+            child: dataBody(),
+          ),
+        ],
+      ),
+    );
   }
 }
